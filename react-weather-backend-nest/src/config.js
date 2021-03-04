1ea512a6c3c742dc3eb354e34bfdab33971b523d
Eylon Malin <EylonMalin 6641032+eylonmalin@users.noreply.github.com>
2021-03-04 18:09:34 +0200

    get weather and forecast via proxy

--- react-weather-backend-nest/src/config.js
+++ react-weather-backend-nest/src/config.js
@@ -7,6 +7,8 @@ dotenv.config({ path: envFile });
 const appConfig = {
   weatherUrl: 'https://api.openweathermap.org/data/2.5/weather',
   forecastUrl: 'https://api.openweathermap.org/data/2.5/forecast',
+  proxyHost: process.env.PROXY_HOST,
+  proxyPort: process.env.PROXY_PORT,
 };
 
 export default appConfig;
--- react-weather-backend-nest/src/weather/weather.service.ts
+++ react-weather-backend-nest/src/weather/weather.service.ts
@@ -1,20 +1,41 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import appConfig from '../config';
 import { secrets } from '../secrets';
+import { AxiosRequestConfig } from 'axios';
+import * as tunnel from 'tunnel';
 
 @Injectable()
 export class WeatherService {
-  constructor(private httpService: HttpService) {}
+  private requestConfig: AxiosRequestConfig;
+
+  private tunnel = tunnel.httpsOverHttp({
+    proxy: {
+      host: appConfig.proxyHost,
+      port: +appConfig.proxyPort,
+    },
+  });
+
+  constructor(private httpService: HttpService) {
+    this.requestConfig = appConfig.proxyHost
+      ? {
+          httpsAgent: this.tunnel,
+        }
+      : {};
+  }
 
   async getWeather(city: string): Promise<any> {
     const url = `${appConfig.weatherUrl}?q=${city}&units=metric&APPID=${secrets.appId}`;
-    const result = await this.httpService.get(url).toPromise();
+    const result = await this.httpService
+      .get(url, this.requestConfig)
+      .toPromise();
     return result.data;
   }
 
   async getForcast(city: string): Promise<any> {
     const url = `${appConfig.forecastUrl}?q=${city}&units=metric&APPID=${secrets.appId}`;
-    const result = await this.httpService.get(url).toPromise();
+    const result = await this.httpService
+      .get(url, this.requestConfig)
+      .toPromise();
     return result.data;
   }
 }
