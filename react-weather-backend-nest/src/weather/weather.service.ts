591fdad672b42226821f7835a10060b9027cb8b8
Eylon Malin <6641032+eylonmalin@users.noreply.github.com>
2021-03-04 17:17:03 +0200

    support .env for config

--- react-weather-backend-nest/src/weather/weather.service.ts
+++ react-weather-backend-nest/src/weather/weather.service.ts
@@ -1,21 +1,20 @@
-import {HttpService, Injectable} from '@nestjs/common'
-import {appConfig} from '../config'
-import {secrets} from "../secrets";
+import { HttpService, Injectable } from '@nestjs/common';
+import appConfig from '../config';
+import { secrets } from '../secrets';
 
 @Injectable()
 export class WeatherService {
+  constructor(private httpService: HttpService) {}
 
-    constructor(private httpService: HttpService) {}
+  async getWeather(city: string): Promise<any> {
+    const url = `${appConfig.weatherUrl}?q=${city}&units=metric&APPID=${secrets.appId}`;
+    const result = await this.httpService.get(url).toPromise();
+    return result.data;
+  }
 
-    async getWeather(city:string): Promise<any> {
-        const url = `${appConfig.weatherUrl}?q=${city}&units=metric&APPID=${secrets.appId}`
-        const result = await this.httpService.get(url).toPromise()
-        return result.data
-    }
-
-    async getForcast(city:string): Promise<any> {
-        const url = `${appConfig.forecastUrl}?q=${city}&units=metric&APPID=${secrets.appId}`
-        const result = await this.httpService.get(url).toPromise()
-        return result.data
-    }
+  async getForcast(city: string): Promise<any> {
+    const url = `${appConfig.forecastUrl}?q=${city}&units=metric&APPID=${secrets.appId}`;
+    const result = await this.httpService.get(url).toPromise();
+    return result.data;
+  }
 }
