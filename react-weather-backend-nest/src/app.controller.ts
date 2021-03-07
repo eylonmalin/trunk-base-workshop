41b5d183fce54cc26e44d2dc3b698576dd07f395
Eylon Malin <EylonMalin 6641032+eylonmalin@users.noreply.github.com>
2021-03-07 11:01:35 +0200

    controller use last city if not provided (default Arad,IL)

--- react-weather-backend-nest/src/app.controller.ts
+++ react-weather-backend-nest/src/app.controller.ts
@@ -3,15 +3,26 @@ import {WeatherService} from "./weather/weather.service";
 
 @Controller()
 export class AppController {
+
+  private lastCity: string = 'Arad,IL'
+
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get("/weather")
-  async weather(@Query('city') city:string): Promise<any> {
-    return this.weatherService.getWeather(city);
+  async weather(@Query('city') city?:string): Promise<any> {
+    const actualCity = this.getAndStoreLastCity(city);
+    return this.weatherService.getWeather(actualCity);
+  }
+
+  private getAndStoreLastCity(city: string) {
+    const actualCity = city ?? this.lastCity;
+    this.lastCity = actualCity;
+    return actualCity;
   }
 
   @Get("/forecast")
-  async forecast(@Query('city') city:string): Promise<any> {
-    return this.weatherService.getForcast(city);
+  async forecast(@Query('city') city?:string): Promise<any> {
+    const actualCity = this.getAndStoreLastCity(city);
+    return this.weatherService.getForcast(actualCity);
   }
 }
