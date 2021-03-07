e907edf13e2519cbb52d12914beedefc69461621
Eylon Malin <EylonMalin 6641032+eylonmalin@users.noreply.github.com>
2021-03-07 11:01:35 +0200

    getWeather and getForecast at controller level

--- react-weather-backend-nest/src/app.controller.spec.ts
+++ react-weather-backend-nest/src/app.controller.spec.ts
@@ -1,11 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import * as request from 'supertest';
-import {INestApplication} from "@nestjs/common";
-import {AppModule} from "./app.module";
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 describe('AppController', () => {
-
   let weatherApp: INestApplication;
 
   beforeEach(async () => {
@@ -18,29 +17,64 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
+    function commonAssertForWeather(res: request.Response) {
+      expect(res.body).toHaveProperty('clouds');
+      expect(res.body).toHaveProperty('coord');
+      expect(res.body).toHaveProperty('weather');
+    }
 
     it('/weather (GET) with city', async () => {
-
       //when
-      const res: request.Response = await request(weatherApp.getHttpServer()).get(`/weather?city=Lod`);
+      const res: request.Response = await request(
+        weatherApp.getHttpServer(),
+      ).get(`/weather?city=Lod`);
 
       //then
-      expect(res.body).toHaveProperty("clouds");
-      expect(res.body).toHaveProperty("coord");
-      expect(res.body).toHaveProperty("weather");
-
+      commonAssertForWeather(res);
     });
 
-    it('/forecast (GET) with city=Lod', async () => {
+    it('/weather (GET) without city use last city', async () => {
+      //given
+      await request(weatherApp.getHttpServer()).get(`/weather?city=Lod`);
 
       //when
-      const res: request.Response = await request(weatherApp.getHttpServer()).get(`/forecast?city=Lod`);
+      const res: request.Response = await request(
+        weatherApp.getHttpServer(),
+      ).get(`/weather`);
 
       //then
-      expect(res.body).toHaveProperty("city");
-      expect(res.body).toHaveProperty("list");
-      expect(res.body).toHaveProperty("cod");
+      commonAssertForWeather(res);
+      expect(res.body.name).toEqual('Lod');
+    });
 
+    function commonAssertForForcast(res: request.Response) {
+      expect(res.body).toHaveProperty('city');
+      expect(res.body).toHaveProperty('list');
+      expect(res.body).toHaveProperty('cod');
+    }
+
+    it('/forecast (GET) with city=Lod', async () => {
+      //when
+      const res: request.Response = await request(
+        weatherApp.getHttpServer(),
+      ).get(`/forecast?city=Lod`);
+
+      //then
+      commonAssertForForcast(res);
+    });
+
+    it('/forecast (GET) without city use last city', async () => {
+      //given
+      await request(weatherApp.getHttpServer()).get(`/forecast?city=Lod`);
+
+      //when
+      const res: request.Response = await request(
+        weatherApp.getHttpServer(),
+      ).get(`/forecast`);
+
+      //then
+      commonAssertForForcast(res);
+      expect(res.body.city.name).toEqual('Lod');
     });
   });
 });
