34e5f80c4443690bf7f782bc3a736779e1b6b919
Eylon Malin <EylonMalin 6641032+eylonmalin@users.noreply.github.com>
2021-05-05 10:45:34 +0300

    backend - extract FavoritesService abstraction (Step 1)

--- react-weather-backend-nest/src/favorites/favorites.service.ts
+++ react-weather-backend-nest/src/favorites/favorites.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+
+export interface IFavoritesService {
+  setLastCity(city: string): void;
+
+  getLastCity(): any;
+}
 
 @Injectable()
-export class FavoritesService {
+export class FavoritesService implements IFavoritesService {
   private lastCity: string = 'Arad,IL';
 
   setLastCity(city: string) {
