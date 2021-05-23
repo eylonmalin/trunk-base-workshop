6c3d6de2d7037e8d0342c788e01dff5f3a9e49e8
Eylon Malin <EylonMalin 6641032+eylonmalin@users.noreply.github.com>
2021-05-23 22:16:24 +0300

    frontend - add emoji icons provider

--- react-weather-frontend/src/components/ProvideIcon.js
+++ react-weather-frontend/src/components/ProvideIcon.js
@@ -1,7 +1,13 @@
 import * as weatherIcons from "../weather-icons.json";
+import * as emojiIcons from "../emoji-icons.json";
 
 export const weatherIconsProvider = icon_id => {
-    const prefix = "wi wi-";
-    return prefix + weatherIcons.default[icon_id].icon;
+    if (true) {
+        const prefix = "em em-";
+        return prefix + emojiIcons.default[icon_id].icon;
+    } else {
+        const prefix = "wi wi-";
+        return prefix + weatherIcons.default[icon_id].icon;
+    }
 };
 
